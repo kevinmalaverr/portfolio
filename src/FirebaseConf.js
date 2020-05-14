369ecf2ccf3faf4240e92730d050037577ff9672
kevinmalaverr <kevinmalaverr@gmail.com>
2020-05-14 17:00:27 -0500

    firebase

---
+++ src/FirebaseConf.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-KRG6fTIcC1JgdnALD28fwRpO4gGcPJY",
+  authDomain: "portfolio-ddcf3.firebaseapp.com",
+  databaseURL: "https://portfolio-ddcf3.firebaseio.com",
+  projectId: "portfolio-ddcf3",
+  storageBucket: "portfolio-ddcf3.appspot.com",
+  messagingSenderId: "59067678152",
+  appId: "1:59067678152:web:8f5790d971e779766e54fd"
+};
+// Initialize Firebase
+
+
+export default firebaseConfig
